41a036d8d74ade48477af31adcd00983ed523c06
srajan-code-heaven <34805670+srajan-code-heaven@users.noreply.github.com>
2022-01-06 00:00:28 -0800

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,1 @@
-const awsAccessKey = "AKIA6KDS4SHUBLQUX345";
-const awsSecretKey = "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";
 
-const config = {
-    mongoDB: 'mongodb+srv://root:Surana1028@cluster0.edf0c.mongodb.net/UberEats?retryWrites=true&w=majority'
-};
-
-module.exports = config;
